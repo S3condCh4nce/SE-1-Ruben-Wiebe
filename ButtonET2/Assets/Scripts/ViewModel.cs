050bce2808a249eb7de96e4b0c226d1cb43fd48d
MortalisVictrix <rjuithol@hotmail.nl>
2017-10-12 13:29:02 +0200

    Unity Project Buttons
    Scene1
    2 buttons, print Debug text met button sprites

---
+++ ButtonET2/Assets/Scripts/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ViewModel : MonoBehaviour {
+
+    public void testButton_Click(){
+        Debug.Log("Did I do it?");
+       }
+
+    public void confirmButton_Click()
+    {
+        Debug.Log("Yes, you have, Young Student.");
+    }
+}
+
+
+
+        
