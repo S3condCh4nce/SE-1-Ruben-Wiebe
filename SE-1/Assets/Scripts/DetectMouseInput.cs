852f22024e0d5117d4d039eef6f4e3167324ac04
MortalisVictrix <rjuithol@hotmail.nl>
2017-10-25 10:30:16 +0200

    Deleted the Start() function

--- SE-1/Assets/Scripts/DetectMouseInput.cs
+++ SE-1/Assets/Scripts/DetectMouseInput.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectMouseInput : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	//Detects what Mouse Button is pressed
 	public void MouseInput() {
